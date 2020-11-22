137502fcbc5677e5adeec6b0b8ccb2861d6a7f6b
nilbahaaeddine <nilbahaaeddine@gmail.com>
2020-11-23 00:18:32 +0100

    Fixed various results bugs

--- src/SPARQLRawParser.java
+++ src/SPARQLRawParser.java
@@ -126,19 +126,19 @@ public class SPARQLRawParser {
 				if(whichIndex.charAt(2) == 's') {
 					try {
 						for( Integer s : Dictionary.getInstance().getStore(whichIndex).get(propertyID).get(objectID) )
-							resultPattern.add("(" + whichIndex.charAt(2) + ";" + patterns.get(i).getSubjectVar().getName() + ")" + getIndividual(s));
+							resultPattern.add(whichIndex.charAt(2) + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s));
 					} catch (Exception e) {
 					}
 				} if(whichIndex.charAt(2) == 'p') {
 					try {
 						for( Integer p : Dictionary.getInstance().getStore(whichIndex).get(subjectID).get(objectID) )
-							resultPattern.add("(" + whichIndex.charAt(2) + ";" + patterns.get(i).getPredicateVar().getName() + ")" + getIndividual(p));
+							resultPattern.add(whichIndex.charAt(2) + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p));
 					} catch (Exception e) {
 					}
 				} else if(whichIndex.charAt(2) == 'o') {
 					try {
 						for( Integer o : Dictionary.getInstance().getStore(whichIndex).get(subjectID).get(propertyID) )
-							resultPattern.add("(" + whichIndex.charAt(2) + ";" + patterns.get(i).getObjectVar().getName() + ")" + getIndividual(o));
+							resultPattern.add(whichIndex.charAt(2) + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o));
 					} catch (Exception e) {
 					}
 				}
@@ -147,17 +147,13 @@ public class SPARQLRawParser {
 					if(whichIndex.charAt(1) == 'p') {//object ops
 						for( Object p : Dictionary.getInstance().getStore(whichIndex).get(objectID).keySet().toArray() ) {
 							for( Object s : Dictionary.getInstance().getStore(whichIndex).get(objectID).get(p) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getSubjectVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getPredicateVar().getName() + ")" + getIndividual(s) + "\t" + getIndividual(p));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getSubjectVar().getName() + ";" + "" + ")" + getIndividual(s) + );
-								//System.out.println(getIndividual(s) + "\t" + getIndividual(p));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p));
 							}
 						}
 					} else if(whichIndex.charAt(1) == 'o') {//property pos
 						for( Object o : Dictionary.getInstance().getStore(whichIndex).get(propertyID).keySet().toArray() ) {
 							for( Object s : Dictionary.getInstance().getStore(whichIndex).get(propertyID).get(o) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getSubjectVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getObjectVar().getName() + ")" + getIndividual(s) + "\t" + getIndividual(o));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getSubjectVar().getName() + ";" + "" + ")" + getIndividual(s) + );
-								//System.out.println(getIndividual(s) + "\t" + getIndividual(o));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o));
 							}
 						}
 					}
@@ -166,17 +162,13 @@ public class SPARQLRawParser {
 					if(whichIndex.charAt(1) == 's') {//object osp
 						for( Object s : Dictionary.getInstance().getStore(whichIndex).get(objectID).keySet().toArray() ) {
 							for( Object p : Dictionary.getInstance().getStore(whichIndex).get(objectID).get(s) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getPredicateVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getSubjectVar().getName() + ")" + getIndividual(p) + "\t" + getIndividual(s));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getPredicateVar().getName() + ";" + "" + ")" + getIndividual(p) + );
-								//System.out.println(getIndividual(p) + "\t" + getIndividual(s));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s));
 							}
 						}
 					} else if(whichIndex.charAt(1) == 'o') {//subject sop
 						for( Object o : Dictionary.getInstance().getStore(whichIndex).get(subjectID).keySet().toArray() ) {
 							for( Object p : Dictionary.getInstance().getStore(whichIndex).get(subjectID).get(o) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getPredicateVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getObjectVar().getName() + ")" + getIndividual(p) + "\t" + getIndividual(o));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getPredicateVar().getName() + ";" + "" + ")" + getIndividual(p) + );
-								//System.out.println(getIndividual(p) + "\t" + getIndividual(o));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o));
 							}
 						}
 					}
@@ -185,28 +177,23 @@ public class SPARQLRawParser {
 					if(whichIndex.charAt(1) == 's') {//property pso
 						for( Object s : Dictionary.getInstance().getStore(whichIndex).get(propertyID).keySet().toArray() ) {
 							for( Object o : Dictionary.getInstance().getStore(whichIndex).get(propertyID).get(s) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getObjectVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getSubjectVar().getName() + ")" + getIndividual(o) + "\t" + getIndividual(s));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getObjectVar().getName() + ";" + "" + ")" + getIndividual(o) + );
-								//System.out.println(getIndividual(o) + "\t" + getIndividual(s));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s));
 							}
 						}
 					} else if(whichIndex.charAt(1) == 'p') {//subject spo
 						for( Object p : Dictionary.getInstance().getStore(whichIndex).get(subjectID).keySet().toArray() ) {
 							for( Object o : Dictionary.getInstance().getStore(whichIndex).get(subjectID).get(p) ) {
-								resultPattern.add("(" + whichIndex.charAt(2)  + "=" + patterns.get(i).getObjectVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getPredicateVar().getName() + ")" + getIndividual(o) + "\t" + getIndividual(p));
-								//resultPattern.add("(" + whichIndex.charAt(2) + "=" + patterns.get(i).getObjectVar().getName() + ";" + "" + ")" + getIndividual(o) + );
-								//System.out.println(getIndividual(o) + "\t" + getIndividual(p));
+								resultPattern.add(whichIndex.charAt(2)  + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p));
 							}
 						}
 					}
 				}
 			} else {
-				System.out.println(whichIndex);
 				int cpt = 0;
 				for( Object s : Dictionary.getInstance().getStore(whichIndex).keySet().toArray() ) {
 					for( Object p : Dictionary.getInstance().getStore(whichIndex).get(s).keySet().toArray() ) {
 						for( Object o : Dictionary.getInstance().getStore(whichIndex).get(s).get(p) ) {
-							resultPattern.add("(" + whichIndex.charAt(0) + "=" + patterns.get(i).getSubjectVar().getName() + ";" + whichIndex.charAt(1) + "=" + patterns.get(i).getPredicateVar().getName() + ";" + whichIndex.charAt(2) + "=" + patterns.get(i).getObjectVar().getName() + ")" + getIndividual(s) + getIndividual(p) + getIndividual(o));
+							resultPattern.add(whichIndex.charAt(0) + "\t" + patterns.get(i).getSubjectVar().getName() + "\t" + getIndividual(s) + "\t" + whichIndex.charAt(1) + "\t" + patterns.get(i).getPredicateVar().getName() + "\t" + getIndividual(p) + "\t" + whichIndex.charAt(2) + "\t" + patterns.get(i).getObjectVar().getName() + "\t" + getIndividual(o));
 						}
 					}	
 				}
