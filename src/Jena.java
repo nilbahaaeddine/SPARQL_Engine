25309a43f5dfbf266efe52e9696fb4f75cbd331f
nilbahaaeddine <nilbahaaeddine@gmail.com>
2020-11-23 04:29:48 +0100

    Refactoring of Jena.java

--- src/Jena.java
+++ src/Jena.java
@@ -14,36 +14,56 @@ import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFactory;
 import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.util.FileManager;
 
 public class Jena {
-	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-		String theQuery = "SELECT ?v0 WHERE { ?v0 <http://schema.org/eligibleRegion> <http://db.uwaterloo.ca/~galuc/wsdbm/Country137> . }";
-		String pathData = "Data/100K.rdfxml";
-
+	public QueryExecution checkJena(Query query, String pathData) {
 		//Create a data model and load file
 		Model model = ModelFactory.createDefaultModel();
 		String dataset = pathData;
 		InputStream in = FileManager.get().open(dataset);
 		model.read(in, null, "RDF/XML"); 
 
-		//Create a query object
-		Query query = QueryFactory.create(theQuery);
-		QueryExecution qexec = QueryExecutionFactory.create(query, model);
+		return QueryExecutionFactory.create(query, model);
+	}
 
-		//Execute query and print result
+	public ResultSet selectJena(Query query, String pathData) {
+		QueryExecution qexec = checkJena(query, pathData);
 		try {
-			//SELECT
-			ResultSet rs = qexec.execSelect();
-			ResultSetFormatter.out(System.out, rs, query);
-			//ASK
-			//boolean rs = qexec.execAsk();
-			//System.out.println("Ask result = " + rs);
+			ResultSet results = qexec.execSelect();
+			results = ResultSetFactory.copyResults(results);
+			return results;
 		} finally {
 			qexec.close();
 		}
+	}
+
+	public boolean askJena(Query query, String pathData) {
+		QueryExecution qexec = checkJena(query, pathData);
+		try {
+			boolean result = qexec.execAsk();
+			return result;
+		} finally {
+			qexec.close();
+		}
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		String theQuery = "SELECT * WHERE {  <http://db.uwaterloo.ca/~galuc/wsdbm/SubGenre54> <http://ogp.me/ns#tag> ?v1 .  ?v0 <http://ogp.me/ns#tag> ?v1 .  ?v0 <http://schema.org/description> ?v3 .  ?v0 <http://schema.org/expires> ?v4 .  }";
+		String pathData = "Data/1M.rdfxml.rdf";
+
+		Query query = QueryFactory.create(theQuery);
+
+		Jena jena = new Jena();
+
+		ResultSet rsSelect = jena.selectJena(query, pathData);
+		ResultSetFormatter.out(System.out, rsSelect, query);
+		
+		boolean rsAsk = jena.askJena(query, pathData);
+		System.out.println("Ask result = " + rsAsk);
 	}
 }
